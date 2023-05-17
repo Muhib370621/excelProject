81bf3bec8c1bccd030f3e9ae286a12f01c471e19
Haya Shahid <hayashahid13dec@gmail.com>
2023-05-17 16:08:35 +0500

    rows read

--- lib/main.dart
+++ lib/main.dart
@@ -55,48 +55,54 @@ class _MyHomePageState extends State<MyHomePage> {
     ByteData data = await rootBundle.load("assets/example.xlsx");
     var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
     var excel = Excel.decodeBytes(bytes);
-
+    List<List<dynamic>> rowsList = [];
 
     for (var table in excel.tables.keys) {
-      // print(excel.tables[table].); //sheet Name
-      print(excel.tables[table]!.maxCols);
-      print(excel.tables[table]!.maxRows);
-      // List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-      List<Data?> innerList = excel.tables[table]!.rows.isNotEmpty ? excel.tables[table]!.rows.first : [];
-      // Workbook workbook = Workbook();
-      // workbook.decode(bytes);
-
-      // List extractedValues = innerList
-      //     .map((data) => data)
-      //     .where((value) => value == 'customer' || value == 'name')
-      //     .toList();
-      // print("customer"+extractedValues.toString());
-      // print(excel.tables[table]!.rows[0]);
-      List<Data> dataModelList = [];
-      // List<String> filteredValues = innerList
-      //     .map((data) => data.toString())
-      //     .toList();
-      // print("filter"+filteredValues.toString());
-
-
-      for (var row in innerList) {
-        dataModelList.add(row!);
-        print(dataModelList.length);
-        // if(row)
-        // print("$row");
+      print(table); //sheet Name
+      // print("rows${excel.tables[table]!.rows}"); //sheet Name
+      for (var row in excel.tables[table]!.rows) {
+        List<dynamic> rowList = [];
+        for (var cell in row) {
+          rowList.add(cell?.value);
+        }
+        rowsList.add(rowList);
       }
-      List<String> names = extractNames(dataModelList);
-
-    }
-  }
-  List<String> extractNames(List<DataModel> dataList) {
-    List<String> names = [];
-
-    for (DataModel data in dataList) {
-      names.add(data.name);
     }
 
-    return names;
+    // Now you have a list of rows
+    // You can access each row as follows
+    for (var row in rowsList) {
+      print(row);
+    }
+// for (var table in excel.tables.keys) {
+    //   // print(excel.tables[table].); //sheet Name
+    //   print(excel.tables[table]!.maxCols);
+    //   print(excel.tables[table]!.maxRows);
+    //   // List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    //   List<Data?> tableHeaderList = excel.tables[table]!.rows.isNotEmpty ? excel.tables[table]!.rows[0] : [];
+    //   List<List<dynamic>> rowsList = [];
+    //
+    //   for (var table in excel.tables.keys) {
+    //     print(table); //sheet Name
+    //
+    //     for (var row in excel.tables[table]!.rows) {
+    //       List<dynamic> rowList = [];
+    //       row.forEach((cell) => rowList.add(cell!.value));
+    //       rowsList.add(rowList);
+    //     }
+    //   }
+    //   // Now you have a list of rows
+    //   // You can access each row as follows
+    //   for (var row in rowsList) {
+    //     print(row);
+    //   }
+    //   List<String> columnList = [];
+    //
+    //   for (var row in tableHeaderList) {
+    //     print(row!.value);
+    //     columnList.add(row.value.toString());
+    //   }
+    // }
   }
 
   @override
